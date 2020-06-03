ece62730295c03e731c10e24efaa63a3090fd382
Vivek Varma <vsquared101@gmail.com>
2020-06-04 02:37:55 +0530

    Updates

---
+++ precache-manifest.71a8629b6e090b6e81964325bce9a916.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ebb11ddc98433724b6d05c1f036928",
+    "url": "/aavya-first-birthday/index.html"
+  },
+  {
+    "revision": "69a66e846f8db2142485",
+    "url": "/aavya-first-birthday/static/css/main.d1469511.chunk.css"
+  },
+  {
+    "revision": "7a11476c47cec2b9fa41",
+    "url": "/aavya-first-birthday/static/js/2.bafbbeba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aavya-first-birthday/static/js/2.bafbbeba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69a66e846f8db2142485",
+    "url": "/aavya-first-birthday/static/js/main.5d21cecb.chunk.js"
+  },
+  {
+    "revision": "c24b4d0bc4090b28deec",
+    "url": "/aavya-first-birthday/static/js/runtime-main.1f4a503f.js"
+  },
+  {
+    "revision": "d0e30ca5b50cc870e42f26a3369a18b2",
+    "url": "/aavya-first-birthday/static/media/aavya.d0e30ca5.jpg"
+  },
+  {
+    "revision": "b48a2dbbddce701d2de2028b20335ccd",
+    "url": "/aavya-first-birthday/static/media/anujdada.b48a2dbb.jpg"
+  },
+  {
+    "revision": "e5586629032dce46ab87b53d6a1f5845",
+    "url": "/aavya-first-birthday/static/media/bhabhi.e5586629.jpg"
+  },
+  {
+    "revision": "f3468cc0f58b2b373dce44b48e175a7a",
+    "url": "/aavya-first-birthday/static/media/coolaavya.f3468cc0.jpg"
+  },
+  {
+    "revision": "7b2817eaa190fe1318bb8323cefc2365",
+    "url": "/aavya-first-birthday/static/media/neetikadidi.7b2817ea.jpg"
+  },
+  {
+    "revision": "6d5a8c4218f067394a7ae3c9c6b1f0da",
+    "url": "/aavya-first-birthday/static/media/pp_bg.6d5a8c42.png"
+  },
+  {
+    "revision": "c8f94136368d88458cdb4d67dfa12ff8",
+    "url": "/aavya-first-birthday/static/media/taujitaiji.c8f94136.jpg"
+  }
+]);
